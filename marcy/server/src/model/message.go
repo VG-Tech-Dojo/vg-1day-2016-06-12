7dbd30fdc6a92373e41bdeac035b18e107c4c2ce
Marcy <marcyyy0612@gmail.com>
2016-06-12 14:26:00 +0900

    1-1_marcy

--- marcy/server/src/model/message.go
+++ marcy/server/src/model/message.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"sort"
+	"time"
 
 	"github.com/pkg/errors"
 )
@@ -14,8 +15,8 @@ type (
 	Message struct {
 		Id   int    `json:"id"`
 		Body string `json:"body"`
-		// CreatedAt string `json:"created_at"` // 1-1. メッセージの投稿時刻
-		// Username  string `json:"user_name"`  // 1-2. ユーザ名
+		CreatedAt string `json:"created_at"` // 1-1. メッセージの投稿時刻
+		Username  string `json:"user_name"`  // 1-2. ユーザ名
 	}
 	Messages []Message
 )
@@ -102,6 +103,7 @@ func NewMessage(body string) (*Message, error) {
 		Id:   id,
 		Body: body,
 		// 1-1. CreatedAt に時刻をセットする
+		CreatedAt : time.Now().String(), 
 		// ヒント: https://golang.org/pkg/time/
 		// 1-2. Username にユーザ名をセットする
 	}, nil
