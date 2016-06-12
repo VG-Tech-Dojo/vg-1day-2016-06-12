55cd42fc62fa0cbe7a617d551302cd884739f845
TomoyaHayakawa <simorgh3196@gmail.com>
2016-06-12 14:19:03 +0900

    mission1-1-iOS_sim

--- sim/iOS-client/OneDayInternshipApp/Message.swift
+++ sim/iOS-client/OneDayInternshipApp/Message.swift
@@ -12,13 +12,15 @@ class Message {
     let identifier: Int?
     let body: String!
     // 1-1 時刻データを扱うためのプロパティ追加
+    let date: String
     
     // 1-2 ユーザ名データを扱うためのプロパティ追加
     
-    init(identifier: Int?, body: String) {
+    init(identifier: Int?, body: String, date: String) {
         self.identifier = identifier
         self.body = body
         // 1-1 時刻データを用いて初期化
+        self.date = date
         
         // 1-2 ユーザ名データを用いて初期化
     }
