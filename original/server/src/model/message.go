a41c8e151f4d0f5a16b507b3b4c404c55e447bf9
Yuta OHWATARI <yuta_ohwatari@voyagegroup.com>
2016-06-12 17:38:18 +0900

    itaya's diff

--- original/server/src/model/message.go
+++ original/server/src/model/message.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"sort"
+	"time"
 
 	"github.com/pkg/errors"
 )
@@ -14,7 +15,7 @@ type (
 	Message struct {
 		Id   int    `json:"id"`
 		Body string `json:"body"`
-		// CreatedAt string `json:"created_at"` // 1-1. メッセージの投稿時刻
+		CreatedAt string `json:"created_at"` // 1-1. メッセージの投稿時刻
 		// Username  string `json:"user_name"`  // 1-2. ユーザ名
 	}
 	Messages []Message
@@ -98,12 +99,18 @@ func NewMessage(body string) (*Message, error) {
 		return nil, err
 	}
 
+	t := time.Now()
+	s := ""
+	s  = t.String()
+
 	return &Message{
 		Id:   id,
 		Body: body,
 		// 1-1. CreatedAt に時刻をセットする
 		// ヒント: https://golang.org/pkg/time/
+		CreatedAt: s, //CreatedAt: time.Now().String()
 		// 1-2. Username にユーザ名をセットする
+		//Username:
 	}, nil
 }
 
